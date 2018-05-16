9f9971683544c58f46aea63af09f3d9250c16a18
yuhaoran <yuhaoran@baidu.com>
2018-05-16 15:31:52 +0800

    init mylib

---
+++ MyLib/Classes/libY.h
@@ -0,0 +1,15 @@
+//
+//  libY.h
+//  libY
+//
+//  Created by yuhaoran on 2017/8/23.
+//  Copyright © 2017年 Baidu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface libY : NSObject
+
+- (void)print1;
+
+@end
---
+++ MyLib/Classes/libY.m
@@ -0,0 +1,18 @@
+//
+//  libY.m
+//  libY
+//
+//  Created by yuhaoran on 2017/8/23.
+//  Copyright © 2017年 Baidu. All rights reserved.
+//
+
+#import "libY.h"
+
+@implementation libY
+
+- (void)print1
+{
+    NSLog(@"11111");
+}
+
+@end
